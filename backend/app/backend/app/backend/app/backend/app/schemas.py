b8cf7e760f3f43ecfbd36062ff6de4c1223240e5
bogdanovbs07 <bogdanovbs07@yandex.ru>
2026-04-27 23:46:11 +0300

    Update schemas with Category and Product relationship

--- backend/app/backend/app/backend/app/backend/app/schemas.py
+++ backend/app/backend/app/backend/app/backend/app/schemas.py
@@ -1,18 +1,42 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
+
+# --- Category Schemas ---
+
+class CategoryBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# --- Product Schemas ---
 
 class ProductBase(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     image_url: Optional[str] = None
-    category: Optional[str] = "Gadget"
+    category_id: Optional[int] = None
+
 
 class ProductCreate(ProductBase):
     pass
 
+
 class Product(ProductBase):
     id: int
+    category: Optional[Category] = None  # Вложенная категория
 
     class Config:
         orm_mode = True
